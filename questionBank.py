8f3ea5512fba6db96fe5f3d0d94e0a075010b187
Sachin Honnudike <shonnudi@student.umass.edu>
2013-04-10 22:18:47 -0400

    Add two functions
    Add two functions

--- questionBank.py
+++ questionBank.py
@@ -4,6 +4,8 @@ class QuestionBank:
 	databaseLocation = ""
 	connection = ""
 	cursorObj = ""
+	randomHour = -1
+	randomMinute = -1
 
 	def __init__ (self,pathToDB):
 		global databaseLocation
@@ -17,21 +19,33 @@ class QuestionBank:
 		global databaseLocation
 		print databaseLocation
 
-	def getTime(self, difficulty=None):
+	def generateTime(self, difficulty=None):
+		global randomHour
+		global randomMinute
+
 		if difficulty is None:
 			cursorObj.execute('SELECT * FROM questionBank ORDER BY RANDOM() LIMIT 1')
 			row = cursorObj.fetchone()
-			hour = str(row[1])
-			minute = str(row[2])
-			if minute == 0 or minute ==5:
-				minute = "0" + minute
-			return hour + "," + minute
+			randomHour = row[1]
+			randomMinute = row[2]
 		else:
 			d = (difficulty,)
 			cursorObj.execute('SELECT * FROM questionBank WHERE difficulty=? ORDER BY RANDOM() LIMIT 1', d)
 			row = cursorObj.fetchone()
-			hour = str(row[1])
-			minute = str(row[2])
-			if minute == 0 or minute ==5:
-				minute = "0" + minute
-			return hour + "," + minute
+			randomHour = str(row[1])
+			randomMinute = str(row[2])
+
+	def getTimeString(self):
+		global randomHour
+		global randomMinute
+		hour = str(randomHour)
+		minute = str(row[2])
+		if minute == 0 or minute ==5:
+			minute = "0" + minute
+		return hour + "," + minute
+
+	def getTimeTouple(self):
+		global randomHour
+		global randomMinute
+		timeTouple = randomHour, randomMinute
+		return timeTouple
