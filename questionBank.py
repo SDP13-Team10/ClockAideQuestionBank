3cbcc468b955ef655a469988e2afe5685342da67
jjeancla <pi@raspberrypi.(none)>
2013-04-10 21:35:54 -0400

    Changes made from Pi

--- questionBank.py
+++ questionBank.py
@@ -1,16 +1,16 @@
 import sqlite3
 
 class QuestionBank:
-	global databaseLocation = ""
-	global connection = ""
-	global cursorObj = ""
+	databaseLocation = ""
+	connection = ""
+	cursorObj = ""
 
-	def __init__ (pathToDB):
+	def __init__ (self,pathToDB):
 		global databaseLocation
 		global connection
 		global cursorObj
 		databaseLocation = pathToDB
-		connection = sqllite3.connect(databaseLocation)
+		connection = sqlite3.connect(databaseLocation)
 		cursorObj = connection.cursor()
 
 	def printDatabaseLocation(self):
@@ -21,20 +21,17 @@ class QuestionBank:
 		if difficulty is None:
 			cursorObj.execute('SELECT * FROM questionBank ORDER BY RANDOM() LIMIT 1')
 			row = cursorObj.fetchone()
-			hour = row[1]
-			minute = row[2]
-			if minute == 0 or minute ==5:
-				minute = str("0" + row[2])
 			hour = str(row[1])
+			minute = str(row[2])
+			if minute == 0 or minute ==5:
+				minute = "0" + minute
 			return hour + "," + minute
 		else:
 			d = (difficulty,)
 			cursorObj.execute('SELECT * FROM questionBank WHERE difficulty=? ORDER BY RANDOM() LIMIT 1', d)
 			row = cursorObj.fetchone()
-			row = cursorObj.fetchone()
-			hour = row[1]
-			minute = row[2]
-			if minute == 0 or minute ==5:
-				minute = str("0" + row[2])
 			hour = str(row[1])
+			minute = str(row[2])
+			if minute == 0 or minute ==5:
+				minute = "0" + minute
 			return hour + "," + minute
